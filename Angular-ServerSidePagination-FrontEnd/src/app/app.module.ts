a4f0aee751c13095263d23ee1de5feee9843a1b8
Sullivan008 <kozakpeter0424@gmail.com>
2020-07-13 09:06:43 +0200

    - Implement the FrontEnd source code into the project. - The source code for FrontEnd includes:   - Installed AgGrid Community Component.   - Installed Bootstrap 4.5.   - Separate IoC Container Configurations.   - Implement and register and using AutoMapper in ASP.NET Core.   - Implement Business Logic through Business Engines.   - Implement Internal Business Logic through Business Services.   - Implement Queryable Extension.

---
+++ Angular-ServerSidePagination-FrontEnd/src/app/app.module.ts
@@ -0,0 +1,14 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AgGridModule } from 'ag-grid-angular';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, AgGridModule.withComponents([])],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
